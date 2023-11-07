e5545109b26e4d73557778dc82b2eddb8abe332e
WeazNet <vladalexandru.tanasov@gmail.com>
2023-11-07 14:26:57 +0100

    first commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,13 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
+import { HelloMessage } from './helloMessage';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get("/hello/:name")
+  getHello(@Param("name") name:string): HelloMessage {
+    return this.appService.getHello(name);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { HelloMessage } from './helloMessage';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello(name :string): HelloMessage {
+    return {"hello":name};
   }
 }
